87546d005e68bc7982fae4dcf4757fc84ac33bda
bagasjiwanta <bagasjiwanta@gmail.com>
2023-02-02 17:28:58 +0700

    fix import error

--- src/components/Logic.jsx
+++ src/components/Logic.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { extendedVigenereFile, vigenereDecrypt, vigenereEncrypt, } from "../functions/viginere";
+import { extendedVigenereFile, vigenereDecrypt, vigenereEncrypt, } from "../functions/vigenere";
 
 const Context = createContext()
 
